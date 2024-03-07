a8f94d522035a7a11f8e9ee4255e4e60f6ac380c
tq <yumohc@163.com>
2024-03-07 22:33:29 +0800

    socks5 setup

--- socks5-setup.sh
+++ socks5-setup.sh
@@ -5,7 +5,7 @@ wget https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.18.1/$
 tar -xvf ${file}
 
 address="0.0.0.0:443"
-./ssserver -s ${address} -m aes-256-gcm -k 123321eq --dns 8.8.8.8 --dns-cache-size 10000 --tcp-fast-open --tcp-no-delay --worker-threads 2 -d
+./ssserver -s ${address} -m aes-256-gcm -k test123 --dns 8.8.8.8 --dns-cache-size 10000 --tcp-fast-open --tcp-no-delay --worker-threads 2 -d
 address="[::]:443"
-./ssserver -s ${address} -m aes-256-gcm -k 123321eq --dns 8.8.8.8 --dns-cache-size 10000 --tcp-fast-open --tcp-no-delay --worker-threads 2 -d
+./ssserver -s ${address} -m aes-256-gcm -k test123 --dns 8.8.8.8 --dns-cache-size 10000 --tcp-fast-open --tcp-no-delay --worker-threads 2 -d
 ps -aux | grep ssserver
